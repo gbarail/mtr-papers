65d66f2b3da9c4933b5d701efa0d6136b9ed4523
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-02-09 14:07:31 +0800

    build: use esbuild `serve`

--- docs/src/ts/main/esbuild-watch.ts
+++ docs/src/ts/main/esbuild-watch.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import { context } from 'esbuild';
 
 import { buildOptions } from '../esbuild/config';
@@ -5,7 +7,12 @@ import { buildOptions } from '../esbuild/config';
 (async () => {
   try {
     const ctx = await context(buildOptions);
-    await ctx.watch();
+
+    const { host, port } = await ctx.serve({
+      host: 'localhost',
+      servedir: path.join(__dirname, '..', '..', '..'),
+    });
+    console.info(`Serving on http://${host}:${port}`);
   } catch (error) {
     console.error(error);
   }
