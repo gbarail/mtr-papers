e0af0ed0b83aaae2b9f255bbd5f0b9231b56f6ae
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-02-09 15:21:21 +0800

    chore: simplify `copyPapers`

--- docs/src/ts/esbuild/prebuild.ts
+++ docs/src/ts/esbuild/prebuild.ts
@@ -9,17 +9,5 @@ function copyPapers() {
   const papersDir = path.resolve(__dirname, '../../../../papers');
   const outDir = path.resolve(__dirname, '../../../dist/papers');
 
-  const papers = fs.readdirSync(papersDir, {
-    recursive: true,
-    withFileTypes: true,
-  }).filter(dirent => dirent.isFile());
-
-  papers.forEach(dirent => {
-    const srcPath = path.join(dirent.path, dirent.name);
-
-    const destDir = path.join(outDir, path.relative(papersDir, dirent.path));
-    fs.mkdirSync(destDir, { recursive: true });
-
-    fs.copyFileSync(srcPath, path.join(destDir, dirent.name));
-  });
+  fs.cpSync(papersDir, outDir, { recursive: true });
 }
