863fb50cd2b0f49e55886df62b8b670192c0adb1
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-02-09 13:35:54 +0800

    chore: show error in `esbuild-watch.ts`

--- docs/src/ts/main/esbuild-watch.ts
+++ docs/src/ts/main/esbuild-watch.ts
@@ -3,6 +3,10 @@ import { context } from 'esbuild';
 import { buildOptions } from '../esbuild/config';
 
 (async () => {
-  const ctx = await context(buildOptions);
-  await ctx.watch();
+  try {
+    const ctx = await context(buildOptions);
+    await ctx.watch();
+  } catch (error) {
+    console.error(error);
+  }
 })();
